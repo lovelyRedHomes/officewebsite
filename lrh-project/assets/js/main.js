14f1bee344356cdf77b25056317d295da7fe694b
lovelyRedHomes <chandrashekharlrh@gmail.com>
2024-04-09 13:38:24 +0530

    today second commit

--- lrh-project/assets/js/main.js
+++ lrh-project/assets/js/main.js
@@ -212,3 +212,10 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+// select counrty code
+
+var input = document.querySelector("#phone");
+window.intlTelInput(input, {
+  separateDialCode: true
+})
